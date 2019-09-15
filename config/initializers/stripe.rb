1cb3af8ebdab7f174cb613eec502e7e4c5a5c655
ryanwmartin <ryanwmartin@gmail.com>
2019-09-15 03:43:58 -0400

    Stripe revert to ENV not Rails credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,10 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.stripe[:publishable_key],
-  :secret_key      => Rails.application.credentials.stripe[:secret_key]
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 
-# ENV['PUBLISHABLE_KEY'],
-# ENV['SECRET_KEY']
+# Rails.application.credentials.stripe[:publishable_key],
+# Rails.application.credentials.stripe[:secret_key]
 
 # Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
