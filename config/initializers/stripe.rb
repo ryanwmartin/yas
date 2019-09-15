22e317ab43a8d2d97da225c3cfe3ca84a6572282
ryanwmartin <ryanwmartin@gmail.com>
2019-09-15 03:36:35 -0400

    UI changes, revert to stripe rails config settings

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,10 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => Rails.application.credentials.stripe[:publishable_key],
+  :secret_key      => Rails.application.credentials.stripe[:secret_key]
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# ENV['PUBLISHABLE_KEY'],
+# ENV['SECRET_KEY']
+
+Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
